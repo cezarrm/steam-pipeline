1ecc6ddce7f7affa892281ab4089add30672584f
Cezar Miranda <cezar611@gmail.com>
2026-01-29 23:20:11 -0300

    novas funçõe

--- extract/steam_api.py
+++ extract/steam_api.py
@@ -12,19 +12,33 @@ STEAM_ID = os.getenv("STEAM_ID")
 URL = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/"
 
 
+def get_owned_games(): 
+        params = {
+                "key": STEAM_API_KEY,
+                "steamid" : STEAM_ID,
+                "include_appinfo": True,
+                "include_played_free_games": True,
+                "format": "json"
+            }
+        
+        print('Chamando a API...')
+        response = requests.get(URL, params=params)
+        response.raise_for_status()
+        return response.json()
 
-params = {
-        "key": STEAM_API_KEY,
-        "steamid" : STEAM_ID,
-        "include_appinfo": True,
-        "include_played_free_games": True,
-        "format": "json"
-    }
-print('Chamando a API...')
-response = requests.get(URL, params=params)
-response.raise_for_status()
+def save_raw_json(data):
+        os.makedirs("data/raw", exist_ok=True)
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        path = f'data/raw/owned_games_{timestamp}.json'
 
-data = response.json()
+        with open (path, 'w', encoding="utf-8") as f:
+                   json.dump(data, f, indent=2, ensure_ascii=False)
+        print(f"Raw salvo em {path}")
 
-print("\n Resposta da API:")
-print(data)
+def main():
+        print('Extraindo dados da API')
+        data = get_owned_games()
+        save_raw_json(data)
+
+if __name__ == "__main__":
+    main()
